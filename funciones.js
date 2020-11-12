d919a899bd5b30a451961f360e1c4c1c0b5ab6e1
alexrichard1994 <alexandrorichard@hotmail.com>
2020-11-12 16:59:53 -0500

    comit

---
+++ funciones.js
@@ -0,0 +1,15 @@
+const numerica = 10
+
+const string = "ya casi"
+
+const bool = true
+
+let empty; 
+
+console.log (trypeof numerica)
+
+console.log (typeof bool)
+
+console.log (typeof string)
+
+console.log (typeof empty)
